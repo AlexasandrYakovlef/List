96fcf28e005bc018165e13607034abc8c6d714ee
AlexasandrYakovlef <aleksandar.iack0vlev@mail.ru>
2022-03-24 09:09:38 +0300

    /

--- ChangeTem.js
+++ ChangeTem.js
@@ -11,3 +11,38 @@ function ChangeTema()
         document.body.classList.add("White");
     }
 }
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
+
+function setCookie(name, value, options = {}) {
+
+    options = {
+      path: '/',
+      // при необходимости добавьте другие значения по умолчанию
+      ...options
+    };
+  
+    if (options.expires instanceof Date) {
+      options.expires = options.expires.toUTCString();
+    }
+  
+    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+  
+    for (let optionKey in options) {
+      updatedCookie += "; " + optionKey;
+      let optionValue = options[optionKey];
+      if (optionValue !== true) {
+        updatedCookie += "=" + optionValue;
+      }
+    }
+    document.cookie = updatedCookie;
+}
+function SetTem(){
+    if(getCookie("tem") == undefined)
+        getCookie("tem") == 
+}
